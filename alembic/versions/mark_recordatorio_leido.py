1e401c42c41a45a8dac039303fda955bcc28bead
djdario14 <djdario14doxis@gmail.com>
2026-01-17 20:22:02 -0500

    MIG: Merge de heads Alembic y fix revision en mark_recordatorio_leido

--- alembic/versions/mark_recordatorio_leido.py
+++ alembic/versions/mark_recordatorio_leido.py
@@ -3,6 +3,11 @@ Revision ID: mark_recordatorio_leido
 Revises: 
 Create Date: 2026-01-17
 """
+
+revision = 'mark_recordatorio_leido'
+down_revision = None
+branch_labels = None
+depends_on = None
 from alembic import op
 import sqlalchemy as sa
 
---
+++ alembic/versions/merge_heads_20260117.py
@@ -0,0 +1,19 @@
+"""
+Merge heads: 8bd8d2cc1156, mark_recordatorio_leido
+Create Date: 2026-01-17
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = 'merge_heads_20260117'
+down_revision = ('8bd8d2cc1156', 'mark_recordatorio_leido')
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
